2a43afbaae6bda0b23bca725ab79569aa609184b
qiuweihong <1176101021qiu@gmail.com>
2023-09-22 08:56:10 +0000

    modify qn 1-3

--- lab1/Labsetup/task1-1.py
+++ lab1/Labsetup/task1-1.py
@@ -2,11 +2,11 @@ from scapy.all import *
 
 filterICMP = 'icmp'
 filterTCP = 'src host 10.9.0.5 and dst port 12345'
-filterSubnet  = ' src net x.x.x.x/16'
+filterSubnet  = ' src net 128.230.0.0/16'
 
 def print_pkt(pkt):
     pkt.show()
-pkt = sniff(iface='br-1714b192a2df', filter=filterTCP,prn=print_pkt)
+pkt = sniff(iface='br-1714b192a2df', filter='icmp',prn=print_pkt)
 
 
 
--- lab1/Labsetup/task1-3.py
+++ lab1/Labsetup/task1-3.py
@@ -1,27 +1,29 @@
-# Traceroute
-
-import sys
 from scapy.all import *
+import sys
 
 def traceroute(dest, max_hops=30):
     ttl = 1
     while ttl <= max_hops:
         pkt = IP(dst=dest, ttl=ttl) / ICMP()
-        # The sr1 function sends the packet and returns the answer
         reply = sr1(pkt, verbose=0, timeout=2)
 
         if reply is None:
             print(f"{ttl}. No reply")
-        elif reply.type == 3:  # This is an ICMP Time Exceeded message
+        elif reply.type == 0:
             print(f"{ttl}. {reply.src}")
-        elif reply.type == 0:  # This is an ICMP Echo Reply message
-            print(f"{ttl}. {reply.src} - Reached!")
-            break
-        else:
-            print(f"{ttl}. {reply.src} - Unknown response")
             break
 
+        elif reply.type == 3:
+            if reply.code == 3:
+                print(f"{ttl}. {reply.src} - Port unreachable (Destination reached)")
+                break
+        elif reply.type == 11:  # Time Exceeded
+            print(f"{ttl}. {reply.src}")
+        else:
+            print(f"{ttl}. {reply.src} - Unknown response (Type: {reply.type}, Code: {reply.code})")
+            break
         ttl += 1
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -29,3 +31,4 @@ if __name__ == "__main__":
         sys.exit(1)
     dest = sys.argv[1]
     traceroute(dest)
+
