db1e3ff1105a5dbd5126993d4f8a38f0d9083241
weihong <1176101021qiu@gmail.com>
2023-10-21 23:08:45 +0700

    ICMP and Telnet filter

--- firewall/Files/packet_filter/seedFilter.c
+++ firewall/Files/packet_filter/seedFilter.c
@@ -1,3 +1,4 @@
+#include <linux/icmp.h>
 #include <linux/if_ether.h>
 #include <linux/inet.h>
 #include <linux/ip.h>
@@ -9,10 +10,10 @@
 #include <linux/udp.h>
 
 static struct nf_hook_ops hook2;
-static struct nf_hook_ops nfho[NF_INET_NUMHOOKS]
+static struct nf_hook_ops nfho[NF_INET_NUMHOOKS];
 
 unsigned int blockUDP(void *priv, struct sk_buff *skb,
-             const struct nf_hook_state *state) {
+                      const struct nf_hook_state *state) {
   struct iphdr *iph;
   struct udphdr *udph;
 
@@ -87,24 +88,87 @@ unsigned int printInfo(void *priv, struct sk_buff *skb,
 
   return NF_ACCEPT;
 }
+unsigned int blockPing(void *priv, struct sk_buff *skb,
+                       const struct nf_hook_state *state) {
+  struct iphdr *iph;
+
+  if (!skb)
+    return NF_ACCEPT;
+
+  iph = ip_hdr(skb);
+
+  if (iph->protocol == IPPROTO_ICMP) {
+    struct icmphdr *icmph;
+    icmph = icmp_hdr(skb);
+    // ICMP type for Echo Request is 8
+    if (icmph->type == 8) {
+      printk(KERN_WARNING "*** Dropping %pI4 (ICMP Echo Request)\n",
+             &(iph->saddr));
+      return NF_DROP;
+    }
+  }
+  return NF_ACCEPT;
+}
+
+unsigned int blockTelnet(void *priv, struct sk_buff *skb,
+                         const struct nf_hook_state *state) {
+  struct iphdr *iph;
+  struct tcphdr *tcph;
+
+  u16 port = 23;            // Telnet port
+  char ip[16] = "10.9.0.1"; // VM IP address
+  u32 ip_addr;
+
+  if (!skb)
+    return NF_ACCEPT;
+
+  iph = ip_hdr(skb);
+  in4_pton(ip, -1, (u8 *)&ip_addr, '\0', NULL);
+
+  if (iph->protocol == IPPROTO_TCP) {
+    tcph = tcp_hdr(skb);
+    if (iph->daddr == ip_addr && ntohs(tcph->dest) == port) {
+      printk(KERN_WARNING "*** Dropping %pI4 (TCP), port %d\n", &(iph->daddr),
+             port);
+      return NF_DROP;
+    }
+  }
+  return NF_ACCEPT;
+}
+
+struct nf_hook_ops icmp_block_to_target = {
+    .hook = blockPing,
+    .hooknum = NF_INET_PRE_ROUTING,
+    .pf = PF_INET,
+    .priority = NF_IP_PRI_FIRST,
+};
+
+struct nf_hook_ops telnet_block_to_target = {
+    .hook = blockTelnet,
+    .hooknum = NF_INET_PRE_ROUTING,
+    .pf = PF_INET,
+    .priority = NF_IP_PRI_FIRST,
+};
 
 int registerFilter(void) {
   printk(KERN_INFO "Registering filters.\n");
-  for (int i = 0; i < NF_INET_NUMHOOKS; i++) {
-      nfho[i].hook = printInfo;
-      nfho[i].hooknum = i;
-      nfho[i].pf = PF_INET;
-      nfho[i].priority = NF_IP_PRI_FIRST;
-      
-      nf_register_hook(&nfho[i]);
-  }
+  int i;
+  for (i = 0; i < NF_INET_NUMHOOKS; i++) {
+    nfho[i].hook = printInfo;
+    nfho[i].hooknum = i;
+    nfho[i].pf = PF_INET;
+    nfho[i].priority = NF_IP_PRI_FIRST;
 
+    nf_register_net_hook(&init_net, &nfho[i]);
+  }
 
   hook2.hook = blockUDP;
   hook2.hooknum = NF_INET_POST_ROUTING;
   hook2.pf = PF_INET;
   hook2.priority = NF_IP_PRI_FIRST;
   nf_register_net_hook(&init_net, &hook2);
+  nf_register_net_hook(&init_net, &icmp_block_to_target);
+  nf_register_net_hook(&init_net, &telnet_block_to_target);
 
   return 0;
 }
@@ -112,9 +176,12 @@ int registerFilter(void) {
 void removeFilter(void) {
   printk(KERN_INFO "The filters are being removed.\n");
 
-  for (int i = 0; i < NF_INET_NUMHOOKS; i++) {
-        nf_unregister_hook(&nfho[i]);
+  int i;
+  for (i = 0; i < NF_INET_NUMHOOKS; i++) {
+    nf_unregister_net_hook(&init_net, &nfho[i]);
   }
+  nf_unregister_net_hook(&init_net, &icmp_block_to_target);
+  nf_unregister_net_hook(&init_net, &telnet_block_to_target);
   nf_unregister_net_hook(&init_net, &hook2);
 }
 
